e28472a2aa0729c67f9d6737e5e27205b89900fd
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 07:42:02 +0200

    Update chat_model.py

--- src/models/chat_model.py
+++ src/models/chat_model.py
@@ -1,1 +1,21 @@
+import openai
 
+class ChatModel:
+    def __init__(self, api_key, model="text-davinci-002"):
+        self.api_key = api_key
+        self.model = model
+        openai.api_key = self.api_key
+
+    def get_response(self, prompt):
+        response = openai.Completion.create(
+            engine=self.model,
+            prompt=prompt,
+            max_tokens=50  # Vous pouvez ajuster ce nombre selon vos besoins
+        )
+        return response.choices[0].text.strip()
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    api_key = "votre_clé_API_OpenAI"
+    chat_model = ChatModel(api_key)
+    print(chat_model.get_response("Bonjour, comment vas-tu ?"))
