d839b8bea60cd1352caf150bc2d53fdb4bd8f124
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 07:44:49 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
-from telegram.ext import Updater, CommandHandler
-
-def start(update, context):
-    update.message.reply_text('Bonjour, je suis votre chatbot!')
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from src.config import settings  # Assurez-vous que le chemin d'importation est correct
+from src.database.telegram import telegram_api  # Assurez-vous que le chemin d'importation est correct
+from src.database.telegram import telegram_commands  # Assurez-vous que le chemin d'importation est correct
 
 def main():
-    updater = Updater("TOKEN", use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+    # Initialisez l'API Telegram
+    telegram_api_instance = telegram_api.TelegramAPI()
+
+    # Enregistrez les commandes Telegram
+    telegram_commands.register_commands(telegram_api_instance.dispatcher)
+
+    # Démarrez le bot
+    telegram_api_instance.start_bot()
 
 if __name__ == '__main__':
     main()
