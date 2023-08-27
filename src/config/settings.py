4c06b1daddd3ba5a365336074fdfe5733fadb3aa
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 08:02:06 +0200

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,1 +1,6 @@
-TELEGRAM_API_TOKEN = "Votre_Token_API"
+# Initialisation du token OpenAI
+OPENAI_API_TOKEN = "Votre_Token_API_OpenAI"
+
+def update_openai_api_token(new_token):
+    global OPENAI_API_TOKEN
+    OPENAI_API_TOKEN = new_token
