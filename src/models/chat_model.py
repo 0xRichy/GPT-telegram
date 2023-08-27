7752891fcfbb955457af9c9a5157779d57b2d86c
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 07:53:33 +0200

    Update chat_model.py

--- src/models/chat_model.py
+++ src/models/chat_model.py
@@ -1,4 +1,5 @@
 import openai
+from src.utils import helpers  # Assurez-vous que le chemin d'importation est correct
 
 class ChatModel:
     def __init__(self, api_key, model="text-davinci-002"):
@@ -7,11 +8,21 @@ class ChatModel:
         openai.api_key = self.api_key
 
     def get_response(self, prompt):
+        # Nettoie l'entrée utilisateur
+        prompt = helpers.sanitize_user_input(prompt)
+
+        # Enregistrement du message d'envoi à l'API dans la console
+        helpers.log_message(f"Envoi de la requête à l'API OpenAI avec le prompt : {prompt}")
+
         response = openai.Completion.create(
             engine=self.model,
             prompt=prompt,
             max_tokens=50  # Vous pouvez ajuster ce nombre selon vos besoins
         )
+
+        # Enregistrement du message de réponse de l'API dans la console
+        helpers.log_message(f"Réponse reçue de l'API OpenAI : {response.choices[0].text.strip()}")
+
         return response.choices[0].text.strip()
 
 # Exemple d'utilisation
