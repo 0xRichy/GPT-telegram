a8bc078913c70edc7de7c8e8c5a66074e60eb018
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 07:49:51 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from src.config import settings  # Assurez-vous que le chemin d'importation est correct
 from src.database.telegram import telegram_api  # Assurez-vous que le chemin d'importation est correct
 from src.database.telegram import telegram_commands  # Assurez-vous que le chemin d'importation est correct
+from src.utils import helpers  # Assurez-vous que le chemin d'importation est correct
 
 def main():
+    # Enregistrement du message de démarrage dans la console
+    helpers.log_message("Initialisation du bot Telegram...")
+
     # Initialisez l'API Telegram
     telegram_api_instance = telegram_api.TelegramAPI()
 
@@ -12,6 +16,9 @@ def main():
 
     # Démarrez le bot
     telegram_api_instance.start_bot()
+
+    # Enregistrement du message de fin dans la console
+    helpers.log_message("Bot Telegram démarré.")
 
 if __name__ == '__main__':
     main()
