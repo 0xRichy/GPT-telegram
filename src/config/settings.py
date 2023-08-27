bb55ae1717c35439421debac66f34376adda355c
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 08:04:44 +0200

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,12 @@
 # Initialisation du token OpenAI
 OPENAI_API_TOKEN = "Votre_Token_API_OpenAI"
 
+# Initialisation du token Telegram
+TELEGRAM_API_TOKEN = "Votre_Token_API_Telegram"
+
+# Initialisation du Chat ID Telegram
+TELEGRAM_CHAT_ID = "Votre_Chat_ID_Telegram"
+
 def update_openai_api_token(new_token):
     global OPENAI_API_TOKEN
     OPENAI_API_TOKEN = new_token
