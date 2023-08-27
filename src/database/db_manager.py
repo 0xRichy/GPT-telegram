3f79a111860a6cd4db505ded21c5a2c7db356ee4
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 07:33:03 +0200

    Update db_manager.py

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -1,1 +1,9 @@
+import sqlite3
 
+def create_table():
+    conn = sqlite3.connect('chatbot.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT)''')
+    conn.commit()
+    conn.close()
+  
