5e699f66bcef45ec82ec3f55b6de254ac3d8915e
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-27 07:29:48 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    update.message.reply_text('Bonjour, je suis votre chatbot!')
+
+def main():
+    updater = Updater("TOKEN", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
 
